010ad8a7deca045f6582f18f4a5cf66fbda21b4f
Mritunjay <mritunjay0506@gmail.com>
2020-05-17 10:26:52 +0530

    java application initial setup

---
+++ src/main/java/javaapplication/javaapplication/App.java
@@ -0,0 +1,13 @@
+package javaapplication.javaapplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
